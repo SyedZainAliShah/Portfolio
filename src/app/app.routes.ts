9277474a15a57603ece2a9d7f52eff01e05805f0
Syed Zain Ali Shah <zainali.mcfc@gmail.com>
2025-01-29 05:46:45 +0100

    Added Background Image, Changed About Page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
+import { GameComponent } from './game/game.component';
 
 // Define the routes
 export const routes: Routes = [
@@ -10,5 +11,6 @@ export const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'game', component: GameComponent },
   { path: '**', redirectTo: '' } // Redirect to home for unknown routes
 ];
