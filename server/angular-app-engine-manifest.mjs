7a320a621aecfb925c8c2f4c004b0a6cacf22508
SyedZainAliShah <syedzain.alishah@outlook.com>
2025-10-14 14:19:39 +0200

    Auto-generated commit

---
+++ server/angular-app-engine-manifest.mjs
@@ -0,0 +1,10 @@
+
+export default {
+  basePath: 'https://SyedZainAliShah.github.io/Portfolio',
+  supportedLocales: {
+  "en-US": ""
+},
+  entryPoints: {
+    '': () => import('./main.server.mjs')
+  },
+};
