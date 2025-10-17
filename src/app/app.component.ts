5464bb399e0d1884913e8f52a077722f0aa3f0aa
SyedZainAliShah <syedzain.alishah@outlook.com>
2025-10-17 02:59:24 +0200

    Responsiveness Issues Fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -19,4 +20,13 @@ import { ContactComponent } from './contact/contact.component';
 })
 export class AppComponent {
   title = 'zain-portfolio';
+  isMobileMenuOpen = false;
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  closeMobileMenu() {
+    this.isMobileMenuOpen = false;
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,8 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  name = "Syed Zain Ali Shah"
-  profession = 'Software Developer'
-  interests = 'I’m a computer science enthusiast with a passion for coding, especially when it comes to building sleek and efficient applications using Angular. Outside of tech, I love spending time in my garden, finding peace in nurturing plants and watching them thrive. I’m also deeply inspired by art, whether I’m creating something myself or appreciating the work of others. My interests are a mix of creativity and logic, and I’m always excited to learn, create, and grow—both in tech and in life.'
-
+  profession = 'Software Developer';
 }
