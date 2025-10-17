caa09da11e3e4d34dccb035091a7f72c701b695e
SyedZainAliShah <syedzain.alishah@outlook.com>
2025-10-17 03:59:43 +0200

    Easter Egg

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Import CommonModule
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true, 
+  imports: [CommonModule], 
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   profession = 'Software Developer';
+  clickCount = 0;
+  isFlipped = false; 
+
+  onImageClick() {
+    this.clickCount++;
+
+    if (this.clickCount >= 2) {
+      this.isFlipped = true;
+      setTimeout(() => {
+        this.isFlipped = false;
+        this.clickCount = 0; 
+      }, 3000);
+    }
+  }
 }
